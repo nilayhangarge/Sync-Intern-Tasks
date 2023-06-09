358f383bfb5e667bacbeb9a896f546a3ced900b7
Nilay Hangarge <nilayhangarge@gmail.com>
2023-06-10 01:49:52 +0530

    Modified Task-2 OTP Verification

--- Task-2 OTP Verification/OTPVFN.py
+++ Task-2 OTP Verification/OTPVFN.py
@@ -3,12 +3,12 @@ import smtplib
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()  # TLS security
-server.login('nilayhangarge@gmail.com', 'csexiggdvougflmi')
+server.login('email_id', 'password')
 otp = ''.join([str(random.randint(0, 9)) for i in range(6)])
 msg = 'Hello User, Your OTP is ' + str(otp)
 sender = ''
 receiver = ''
-server.sendmail('nilayhangarge@gmail.com', 'nilaynhangarge@acpce.ac.in', msg)
+server.sendmail('sender email', 'receiver\'s email', msg)
 a = input("Enter Your OTP >> ")
 if a == otp:
     print("Congratulations, Your OTP is Successfully Verified.")
